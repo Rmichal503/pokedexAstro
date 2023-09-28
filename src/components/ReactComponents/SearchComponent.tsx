813869da6a9914ec755e2e8ff600cf05dff4f002
Rmichal503 <rakmichal503@gmail.com>
2023-09-28 21:19:14 +0200

    view transition, rwd

--- src/components/ReactComponents/SearchComponent.tsx
+++ src/components/ReactComponents/SearchComponent.tsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react'
 export default function SearchComponent() {
     const [inputValue, setInputValue] = useState('')
   return (
-    <div className='border-2 border-slate-900  flex flex-col items-center justify-center rounded-md px-4 py-6 space-y-3 w-fit'>
-        <p className='text-xl md:text-3xl font-bold'>Search Pokemon</p>
-        <input className='p-1 bg-pokemon-main-screen text-slate-900 text-center placeholder:text-slate-900 placeholder:text-xs md:placeholder:text-base placeholder:text-center border border-slate-600 w-56 md:w-2/3 rounded-md' placeholder='Type id or name' type='text' onChange={(e)=>{
+    <div className='border-2 xl:border-[4px] border-slate-900 flex flex-col items-center justify-center rounded-lg xl:rounded-xl px-4 py-6 md:px-6 md:py-8 2xl:px-8 2xl:py-10 space-y-3 xl:space-y-6 w-fit'>
+        <p className='text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-bold'>Search Pokemon</p>
+        <input className='p-1 md:px-3 md:py-2 xl:px-5 xl:py-3 2xl:px-8 2xl:py-5 bg-pokemon-main-screen text-slate-900 text-center xl:text-xl 2xl:text-2xl placeholder:text-slate-900 placeholder:text-xs md:placeholder:text-base 2xl:placeholder:text-2xl placeholder:text-center border border-slate-600 w-56 md:w-2/3 rounded-md' placeholder='Type id or name' type='text' onChange={(e)=>{
             e.preventDefault()
             setInputValue(e.target.value.toLowerCase())
         }}/>
-        <a className='px-2 py-1 bg-pokemon-main-red border border-slate-600 rounded-md hover:bg-pokemon-main-red/80 transition-colors duration-200 text-xs md:text-base' href={`pokemon/${inputValue}`}>Search</a>
+        <a className='px-2 py-1 md:px-3 md:py-2 xl:px-8 xl:py-5 bg-pokemon-main-red border border-slate-600 rounded-md hover:bg-pokemon-main-red/80 transition-colors duration-200 text-xs md:text-base xl:text-2xl' href={`pokemon/${inputValue}`}>Search</a>
     </div>
   )
 }
